0ec6182b2def444dcc526d2884b54156029f7829
Akarsh <akarshs27@gmail.com>
2023-09-12 20:22:09 +0530

    BFS

--- data_structure/trees/BST.js
+++ data_structure/trees/BST.js
@@ -43,17 +43,30 @@ class Node {
         if(this.root == null) return false;
         let current = this.root;
         let found = false;
-        while(current && !found) {
-            if(value < current.left) {
+            if(value < current.value) {
                 current = current.left;
-            } else if( value > current.right) {
+            } else if(value > current.value) {
                 current = current.right;
             } else {
                 return true;
             }
-        }
-         return false;
+        return false;
     }
+
+    BFS() {
+      let node = this.root;
+      let queue = [];
+      let data = [];
+      queue.push(node);
+      while(queue.length) {
+            node = queue.shift();
+            data.push(node.value);
+            if(node.left) queue.push(node.left);
+            if(node.right) queue.push(node.right);
+      }
+      return data;
+    }
+
   }
   
   let tree = new BinarySearchTree();
@@ -61,5 +74,5 @@ class Node {
   tree.insert(15);
   tree.insert(5);
   tree.insert(2);
-  tree.contains(4);
+  tree.contains(5);
   console.log(JSON.stringify(tree));
