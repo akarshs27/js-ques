a8d2674b8a98f3f445d7c34f5d8c45bc9c6a4595
Akarsh <akarshs27@gmail.com>
2023-09-13 12:46:16 +0530

    NGE

--- leetcode/next_greater_element.js
+++ leetcode/next_greater_element.js
@@ -23,3 +23,27 @@ function getNextLargerElement(arr) {
   }
   
   console.log(getNextLargerElement([13, 7, 6, 12]));
+
+
+  function printNGE(arr) {
+    let result = new Array(arr.length).fill(-1);
+    let stack = [];
+  
+    // do for each element
+    for (let i = 0; i < arr.length; i++) {
+      // loop till we have a greater element on top or stack becomes empty.
+  
+      // Keep popping elements from the stack smaller than the current
+      // element, and set their next greater element to the current element
+  
+      while (stack.length !== 0 && arr[stack.length - 1] < arr[i]) {
+        result[stack.pop()] = arr[i];
+      }
+  
+      // push current "index" into the stack
+      stack.push(i);
+    }
+  
+    return result;
+  }
+  console.log(printNGE([11, 13, 21, 3]));
