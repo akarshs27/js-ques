c5d50b6f35b305e3da6e9d6b1d88deacd71b369d
Akarsh <akarshs27@gmail.com>
2025-04-28 15:08:45 +0530

    piping

--- polyfills/pipe.js
+++ polyfills/pipe.js
@@ -1,11 +1,23 @@
 //   function1(function2(function3(function4(args))))
 //   pipe(function1,function2,function3)(args)
 
+const pipe =
+  (...functions) =>
+  (args) =>
+    functions.reduce((arg, func) => func(arg), args);
 
-const pipe = (...functions) => (args) => functions.reduce((arg, func) => func(arg), args);
-
-const sum = (...arr) => arr.flat(1).reduce((x , y) => x + y);
+const sum = (...arr) => arr.flat(1).reduce((x, y) => x + y);
 
 const square = (val) => val * val;
 
-console.log(pipe(sum, square)([3,5]));
+console.log(pipe(sum, square)([3, 5]));
+
+const pipe2 = (...fns) => {
+  return (val) => {
+    fns.forEach((fn) => {
+      val = fn(val);
+    });
+
+    return val;
+  };
+};
