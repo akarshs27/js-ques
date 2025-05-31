d185a704931da7d15d2c4d81f1ef3fe015e4c249
Akarsh <akarshs27@gmail.com>
2025-05-31 17:28:14 +0530

    grind 75

---
+++ dsa/grind75/evaluate_reverse_polish_notation.js
@@ -0,0 +1,41 @@
+// You are given an array of strings tokens that represents an arithmetic expression in a Reverse Polish Notation.
+// Evaluate the expression. Return an integer that represents the value of the expression.
+// Note that:
+// The valid operators are '+', '-', '*', and '/'.
+// Each operand may be an integer or another expression.
+// The division between two integers always truncates toward zero.
+// There will not be any division by zero.
+// The input represents a valid arithmetic expression in a reverse polish notation.
+// The answer and all the intermediate calculations can be represented in a 32-bit integer.
+
+// Example 1:
+// Input: tokens = ["2","1","+","3","*"]
+// Output: 9
+// Explanation: ((2 + 1) * 3) = 9
+
+// Example 2:
+// Input: tokens = ["4","13","5","/","+"]
+// Output: 6
+// Explanation: (4 + (13 / 5)) = 6
+
+function evalRPN(tokens) {
+  let stack = [];
+  const operators = {
+    "+": (num1, num2) => num1 + num2,
+    "-": (num1, num2) => num1 - num2,
+    "*": (num1, num2) => num1 * num2,
+    "/": (num1, num2) => Math.trunc(num1 / num2),
+  };
+
+  for (let token of tokens) {
+    if (operators[token]) {
+      let num2 = stack.pop();
+      let num1 = stack.pop();
+      stack.push(operators[token](num1, num2));
+    } else {
+      stack.push(Number(token));
+    }
+  }
+
+  return stack.pop();
+}
---
+++ dsa/grind75/number+_of_islands.js
@@ -0,0 +1,54 @@
+// Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+
+// An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+
+// Example 1:
+
+// Input: grid = [
+//   ["1","1","1","1","0"],
+//   ["1","1","0","1","0"],
+//   ["1","1","0","0","0"],
+//   ["0","0","0","0","0"]
+// ]
+// Output: 1
+// Example 2:
+
+// Input: grid = [
+//   ["1","1","0","0","0"],
+//   ["1","1","0","0","0"],
+//   ["0","0","1","0","0"],
+//   ["0","0","0","1","1"]
+// ]
+// Output: 3
+
+function numberOfIslands(grid) {
+  let count = 0;
+
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      if (grid[i][j] === "1") {
+        count = count + dfs(grid, i, j);
+      }
+    }
+  }
+
+  function dfs(grid, row, col) {
+    if (
+      row < 0 ||
+      row > grid.length - 1 ||
+      col < 0 ||
+      col > grid[row].length - 1 ||
+      grid[row][col] === "0"
+    ) {
+      return;
+    }
+
+    grid[row][col] = "0";
+    dfs(grid, row - 1, col);
+    dfs(grid, row + 1, col);
+    dfs(grid, row, col - 1);
+    dfs(grid, row, col + 1);
+
+    return 1;
+  }
+}
