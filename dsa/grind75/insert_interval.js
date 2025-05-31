4b01e51c332a62a827c8791aa99102047527e681
Akarsh <akarshs27@gmail.com>
2025-05-31 13:47:47 +0530

    week ques

---
+++ dsa/grind75/insert_interval.js
@@ -0,0 +1,58 @@
+// You are given an array of non-overlapping intervals intervals where intervals[i] = [starti, endi] represent the start and the end of the ith interval and intervals is sorted in ascending order by starti. You are also given an interval newInterval = [start, end] that represents the start and end of another interval.
+
+// Insert newInterval into intervals such that intervals is still sorted in ascending order by starti and intervals still does not have any overlapping intervals (merge overlapping intervals if necessary).
+
+// Return intervals after the insertion.
+
+// Note that you don't need to modify intervals in-place. You can make a new array and return it.
+
+// Example 1:
+// Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
+// Output: [[1,5],[6,9]]
+
+// Example 2:
+// Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+// Output: [[1,2],[3,10],[12,16]]
+// Explanation: Because the new interval [4,8] overlaps with [3,5],[6,7],[8,10].
+
+function insertIntervals(intervals, newInterval) {
+  let res = [];
+  let i = 0;
+
+  let start = 0;
+  let end = 1;
+
+  while (i < intervals.length && intervals[i][end] < newInterval[start]) {
+    res.push(intervals[i]);
+    i++;
+  }
+
+  console.log("Res", res);
+
+  while (i < intervals.length && intervals[i][start] <= newInterval[end]) {
+    newInterval[start] = Math.min(newInterval[start], intervals[i][start]);
+    newInterval[end] = Math.max(newInterval[end], intervals[i][end]);
+    i++;
+  }
+
+  res.push(newInterval);
+  while (i < intervals.length) {
+    res.push(intervals[i]);
+    i++;
+  }
+
+  return res;
+}
+
+console.log(
+  insertIntervals(
+    [
+      [1, 2],
+      [3, 5],
+      [6, 7],
+      [8, 10],
+      [12, 16],
+    ],
+    [4, 8]
+  )
+);
