34b95fb0f1e607f2089c72d13b154eef0fc9b0a3
Akarsh <akarshs27@gmail.com>
2025-06-29 20:59:35 +0530

    sorting algo

---
+++ dsa/sort/merge_sort.js
@@ -0,0 +1,27 @@
+const arr = [2, 4, -2, -6, 10];
+
+function mergeSort(arr) {
+  if (arr.length < 2) {
+    return arr;
+  }
+  const mid = Math.floor(arr.length / 2);
+  const leftArr = arr.slice(0, mid);
+  const rightArr = arr.slice(mid);
+  return merge(mergeSort(leftArr), mergeSort(rightArr));
+}
+
+function merge(leftArr, rightArr) {
+  let sortedArr = [];
+  while (leftArr.length && rightArr.length) {
+    if (leftArr[0] <= rightArr[0]) {
+      sortedArr.push(leftArr.shift());
+    } else {
+      sortedArr.push(rightArr.shift());
+    }
+  }
+  return [...sortedArr, ...leftArr, ...rightArr];
+}
+
+console.log(mergeSort(arr));
+
+// Time complexity - Object(nlogn)
